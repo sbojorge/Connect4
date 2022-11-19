40a859d2206eef989bf8324756d45f41712d6563
sbojorge <sara.bojorge@gmail.com>
2022-11-19 06:52:00 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+It takes ASCII text and renders it in ASCII art fonts
+"""
+import pyfiglet
+
+print(pyfiglet.figlet_format('Connect 4', justify='center'))
